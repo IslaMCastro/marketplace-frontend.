53cf1b7f2ebc2a610e2dbc2639a92652f8051d77
IslaMCastro <133907365+IslaMCastro@users.noreply.github.com>
2023-08-30 15:18:10 -0300

    alterações

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import "./Home.css";
+import React from "react";
+
+
+function Home (){
+    return(
+        <div>
+            <h1>Hello World!</h1>
+            <p>Curso de Programação de Sistemas</p>
+        </div>
+    )
+}
+
+export default Home;
